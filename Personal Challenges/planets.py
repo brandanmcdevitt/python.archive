a05e37c0af5e750a8f4f8f785f1e81a6af1230f4
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-11-29 13:25:49 +0000

    Update planets.py with fun facts about Greek and Roman Gods

--- Personal Challenges/planets.py
+++ Personal Challenges/planets.py
@@ -1,18 +1,26 @@
 # a little program to help me memorise the planets in our galaxy
 
-planets = {1:"Mercury",
-           2:"Venus",
-           3:"Earth",
-           4:"Mars",
-           5:"Jupiter",
-           6:"Saturn",
-           7:"Uranus",
-           8:"Neptune"}
+planets = {1:["Mercury", "Hermes", "Messenger God"],
+           2:["Venus", "Aphrodite", "God of Love"],
+           3:["Earth"],
+           4:["Mars", "Ares", "God of War"],
+           5:["Jupiter", "Zues", "Chief God - God of Sky and Thunder"],
+           6:["Saturn"],
+           7:["Uranus"],
+           8:["Neptune", "Poseidon", "God of Sea"]}
 
 for i in range(len(planets)):
     user_input = ""
 
-    while user_input.lower() != planets[i+1].lower():
+    while user_input.lower() != planets[i+1][0].lower():
         user_input = input("What is planet no. {} from the sun?\n".format(i+1))
 
-    print("\n{} is Correct!\n".format(planets[i+1]))
+    print("\n{} is Correct!\n".format(planets[i+1][0]))
+
+    try:
+        if planets[i+1][1]:
+            print("Fun fact: {} was named after the Greek God {}, who is the {}.\n".format(planets[i+1][0],
+                                                                                           planets[i+1][1],
+                                                                                           planets[i+1][2]))
+    except:
+        print()
