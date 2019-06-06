17e9d89956ecb6718391d50021a3c2804d9a7e02
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2019-06-06 22:52:47 +0100

    Update error_handling.py with pep8

--- Udemy/Modern_Python3_Bootcamp/error_handling.py
+++ Udemy/Modern_Python3_Bootcamp/error_handling.py
@@ -2,6 +2,8 @@
 
 # 1. Write a funcion that takes in two strings. Raise errors if the strings are
 #    not given as arguments.
+
+
 def colorize(text, color):
     colors = ("cyan", "yellow", "blue", "green", "magenta")
     if type(color) is not str:
@@ -12,24 +14,31 @@ def colorize(text, color):
         raise ValueError("color is invalid color")
     print(f"Printed {text} in {color}.")
 
-colorize("Hello", "blue") # Printed Hello in blue.
+
+colorize("Hello", "blue")  # Printed Hello in blue.
 
 # 2. Write a function that accepts a dictionary and key and returns None if
 #    there is a KeyError
+
+
 def get(d, key):
     try:
         return d[key]
     except KeyError:
         return None
 
+
 d = {"name": "Ricky"}
-print(get(d, "city")) # None
+print(get(d, "city"))  # None
 
 # 3. Create a function that takes in two numbers, a and b. Return the result of
 #    a/b and raise an error if the user tries to divide by zero or enters a string
-def divide(a,b):
+
+
+def divide(a, b):
     try:
-        import pdb; pdb.set_trace() # Using pdb to step through code
+        import pdb
+        pdb.set_trace()  # Using pdb to step through code
         result = a/b
     except (ZeroDivisionError, TypeError) as err:
         print("Something went wrong.")
@@ -37,4 +46,5 @@ def divide(a,b):
     else:
         print(f"{a} divided by {b} is {result}")
 
-divide(2,0) # Something went wrong. Division by zero
+
+divide(2, 0)  # Something went wrong. Division by zero
