a1bb27322fcef11b438451d570e47377befffee8
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2019-06-18 19:01:03 +0100

    add comments

--- Udemy/Modern_Python3_Bootcamp/scraper.py
+++ Udemy/Modern_Python3_Bootcamp/scraper.py
@@ -17,13 +17,17 @@ with open('Udemy/Modern_Python3_Bootcamp/blog_data.csv', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         articles = soup.findAll('article')
 
+        # for each article on the page, take the title, url and date and write
+        # it to the blog_data.csv file
         for article in articles:
             title = article.find('a').get_text()
             url = article.find('a')['href']
             date = article.find('time')['datetime']
             csv_writer.writerow([title, url, date])
-            
+        
+        # increment by 1 each time to crawl through pagination
         count += 1
         
+        # break out of the loop if no articles are found
         if not articles:
             break
