9b0e89b8b880a9fcde11faefbb70243f0a408630
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2019-06-29 14:38:19 +0100

    Update mac_changer

--- Udemy/Learn_Python_and_Ethical_Hacking_From_Scratch/MAC_changer.py
+++ Udemy/Learn_Python_and_Ethical_Hacking_From_Scratch/MAC_changer.py
@@ -1,3 +1,20 @@
-import subprocess
+# Create a script that allows the user to change the MAC address of a chosen interface
 
-subprocess.call("ifconfig", shell=True)
+import subprocess
+import re
+
+def change_mac_address(interface, mac_address):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+INTERFACE = input("Interface: ")
+MAC = input("MAC Address: ")
+
+mac_regex = re.compile(r'^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$')
+match = mac_regex.search(MAC)
+if match:
+    change_mac_address(INTERFACE, MAC)
+else:
+    print("Sorry, incorrect MAC Address format.")
